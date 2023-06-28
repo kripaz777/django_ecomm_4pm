76b298958af5001403b88b9aed51cc886ffe78f6
Aayush <aceraayush@gmail.com>
2023-06-28 16:22:28 +0545

    models creating

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.CharField(max_length=200,blank = True)
+    slug = models.CharField(max_length=500, unique=True)
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField(blank = True)
+    rank = models.IntegerField()
+    ulr = models.URLField(blank= True, max_length=500)
+    def __str__(self):
+        return self.name
