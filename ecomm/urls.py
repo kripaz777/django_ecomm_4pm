d203e827ae8cc159789f2ad170eb223d39eb22ca
Aayush <aceraayush@gmail.com>
2023-07-11 16:36:36 +0545

    all updated

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
+    path('account/',include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
