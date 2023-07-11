35f60cc45085be581adf6fc57f95ef69d02ade0b
Aayush <aceraayush@gmail.com>
2023-07-11 16:49:33 +0545

    all updated

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -122,5 +122,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
